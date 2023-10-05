144d51174fecda06f8657b7d785edb593a443acb
KaungHsetHEin <kkaunghsethein@gmail.com>
2023-10-05 12:37:40 +0630

    First Commit

---
+++ permissiion-evaluator/src/main/java/com/example/permissiionevaluator/PermissiionEvaluatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.permissiionevaluator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermissiionEvaluatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PermissiionEvaluatorApplication.class, args);
+    }
+
+}
---
+++ permissiion-evaluator/src/test/java/com/example/permissiionevaluator/PermissiionEvaluatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.permissiionevaluator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PermissiionEvaluatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
